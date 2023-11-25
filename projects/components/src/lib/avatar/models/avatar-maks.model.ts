197711c583f2e1d574e876788c537c63d2712232
André Lagacione <lagacioneandre@gmail.com>
2023-11-25 18:57:35 -0500

    test to publish

--- projects/components/src/lib/avatar/models/avatar-maks.model.ts
+++
@@ -1,1 +0,0 @@
-export type AvatarMask = 'hexagon' | 'circle' | 'square';
--- projects/components/src/lib/modal/interfaces/overlay-data.const.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export const OVERLAY_DATA = new InjectionToken<any>('OVERLAY_DATA');
