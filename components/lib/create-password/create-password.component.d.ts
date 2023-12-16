48687ebeae4ad91eba7a10dcdeb1051b61ae35e5
André Lagacione <lagacioneandre@gmail.com>
2023-12-16 18:23:57 -0500

    add validation messages

--- components/lib/create-password/create-password.component.d.ts
+++ components/lib/create-password/create-password.component.d.ts
@@ -2,12 +2,14 @@ import { EventEmitter, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ValidatorFn } from '@angular/forms';
 import { InputType } from './models/input-type';
 import { IPasswordEvent } from './models/password-event';
+import { IMessageValidation } from './models/message-validation';
 import * as i0 from "@angular/core";
 export declare class CreatePasswordComponent implements OnInit, OnDestroy {
     private destroy$;
     passwordValidation: EventEmitter<boolean>;
     submitEvent: EventEmitter<void>;
     passwordEvent: EventEmitter<IPasswordEvent>;
+    messageValidation: IMessageValidation;
     inputConfig: InputType[];
     isPasswordValid: boolean;
     passwordsAreEquals: () => ValidatorFn;
@@ -24,5 +26,5 @@ export declare class CreatePasswordComponent implements OnInit, OnDestroy {
     togglePasswordVisibility(index: number, type: InputType): void;
     submit(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CreatePasswordComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CreatePasswordComponent, "app-create-password", never, {}, { "passwordValidation": "passwordValidation"; "submitEvent": "submitEvent"; "passwordEvent": "passwordEvent"; }, never, never, false, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CreatePasswordComponent, "cdk-create-password", never, { "messageValidation": "messageValidation"; }, { "passwordValidation": "passwordValidation"; "submitEvent": "submitEvent"; "passwordEvent": "passwordEvent"; }, never, never, false, never>;
 }
