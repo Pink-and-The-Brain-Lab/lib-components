2b7d1bee1d785e7744b6fd3fc0fdcbe1870e2a56
André Lagacione <lagacioneandre@gmail.com>
2024-01-03 21:37:02 -0500

    fix unit tests

---
+++ projects/components/src/lib/services/theme-changer.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { ThemeChangerService } from './theme-changer.service';
+
+describe('ThemeChangerService', () => {
+  let service: ThemeChangerService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ThemeChangerService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
