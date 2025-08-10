2b857353b6a56cb8db36f8ecb8d770acb383cfd9
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-08-10 14:51:57 -0300

    move to backup

---
+++ backup/projects/components/src/lib/mocks/http-error-response.test.mock.ts
@@ -0,0 +1,9 @@
+import { HttpErrorResponse } from "@angular/common/http";
+
+const HTTP_ERROR_RESPONSE = {
+    error: {
+        message: 'error message'
+    }
+} as any as HttpErrorResponse;
+
+export default HTTP_ERROR_RESPONSE;
